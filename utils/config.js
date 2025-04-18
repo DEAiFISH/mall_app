2c63577b9a7ea1199cb830ba27388c407145ac71
大鹅爱吃酸菜鱼 <DEAiFISH@Outlook.com>
2025-04-18 17:59:39 +0800

    2025/04/18

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 //可改动页面
 
-var domain = "http://127.0.0.1:10002"; //统一接口域名，测试环境
-
+// var domain = "http://127.0.0.1:10002"; //统一接口域名，测试环境
+var domain = "http://192.168.5.10:10002";
 exports.domain = domain;
 
